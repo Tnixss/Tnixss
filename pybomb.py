92611e6311ef410be8230c7a9eaca669f07b0ef2
Tnixss <147255034+Tnixss@users.noreply.github.com>
2023-10-07 16:59:00 +0300

    Update pybomb.py

--- pybomb.py
+++ pybomb.py
@@ -258,7 +258,7 @@ class SendSms():
         except:
             print(f"{Fore.LIGHTRED_EX}Made By NiqhtBo1 | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> api.yuffi.co")
     #getir
-     def getir(self):
+    def getir(self):
         try:
             wmf = requests.post("https://food-client-api-gateway.getirapi.com/clients/by-gsm/?countryCode=90&gsm=" + self.phone
         if wmf.status_code == 202:
